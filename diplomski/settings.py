1f49b28e3c55f8cc005ab8a1617cb2cd62728412
Ana Ristic <ristic.ana@gmail.com>
2020-03-10 14:45:46 +0100

    Debug false

--- diplomski/settings.py
+++ diplomski/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'nv@js*t)vjj*ude*cb^&e&5_amjlzk
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
